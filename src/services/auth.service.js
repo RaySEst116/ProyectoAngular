8afbb0a1d33c49271ccccac26746e8b147e9d6b0
Ray S Estrada <ray.s.est116@gmail.com>
2026-03-19 09:31:47 -0700

    codigo del token

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -3,5 +3,37 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
 const login = async({ email, password }) => {
-    
+    const user = await prisma.users.findUnique({
+        where : { email }
+    })
+
+    if(!user) {
+        throw new Error('Credenciales invalidas')
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password)
+
+    if(!isPasswordValid){
+        throw new Error('Credenciales invalidas')
+    }
+    const token = jwt.sign(
+        {
+            userId : user.id,
+            email : user.email
+        },
+
+        process.env.JWT_SECRET,
+        { 
+            expires: '1h' 
+        }
+    );
+
+    return {
+        message : 'Login correcto',
+        token
+    }
+}
+
+module.exports = {
+    login
 }
